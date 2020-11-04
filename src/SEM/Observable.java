bc68d76b066e4279cd2771d780aa1fdc8d169940
FerUrquijo <jfurquijo@gmail.com>
2020-11-04 13:17:45 -0300

    tests

---
+++ src/SEM/Observable.java
@@ -0,0 +1,21 @@
+package SEM;
+
+import java.util.ArrayList;
+
+public class Observable {
+	private ArrayList<Observer> observers = new ArrayList<Observer>();
+	
+	public void addObserver(Observer observer) {
+		observers.add(observer);
+	}
+	
+	public void deleteObserver(Observer observer) {
+		observers.remove(observer);
+	}
+	
+	public void notifyObservers(Object param) {
+		for(Observer i : observers) {
+			i.update(param);
+		}
+	}
+}
--- tests/SEM/CentroRegistrosTest.java
+++ tests/SEM/CentroRegistrosTest.java
@@ -1,10 +1,23 @@
 package SEM;
 import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
+class TestCentro extends CentroRegistros{
+
+	protected TestCentro() {
+		super();
+	}
+
+	public ArrayList<Registro> getRegistros(){
+		return this.registros;
+	}
+}
 
 
 public class CentroRegistrosTest {
@@ -23,11 +36,49 @@ public class CentroRegistrosTest {
 	}
 	
 	@Test
-	void testAgregadoDeRegistros() {
+	void testValidacionDeRegistros() {
 		registro1 = mock(Registro.class);
 		when(registro1.getPatente()).thenReturn("abc123");
 		when(registro1.estaVigente()).thenReturn(true);		
 		centroConocido.registrarInicio(registro1);
 		assertTrue(centroConocido.estaVigente("abc123"));
+	}
+	
+	void agregarRegistros() {
+		registro1 = mock(Registro.class);
+		TestCentro centrotest = new TestCentro();
+		centrotest.registrarInicio(registro1);
+		assertTrue(centrotest.getRegistros().contains(registro1));
+	}
+	
+	void finalizarRegistros() {
+		registro1 = mock(Registro.class);
+		when(registro1.getPatente()).thenReturn("abc123");
+		TestCentro centrotest = new TestCentro();
+		centrotest.registrarInicio(registro1);
+		centrotest.registrarFinal("abc123");
+		assertFalse(centrotest.getRegistros().contains(registro1));
+	}
+	
+	
+	@Test
+	void testNotificarObservers() {
+		Observer observer = mock(Observer.class);
+		TestCentro centrotest = new TestCentro();
+		registro1 = mock(Registro.class);
+		centrotest.addObserver(observer);
+		centrotest.registrarInicio(registro1);
+		verify(observer,times(1)).update(centrotest.getRegistros());
+	}
+	
+	@Test
+	void testNotificarObservers2() {
+		Observer observer = mock(Observer.class);
+		TestCentro centrotest = new TestCentro();
+		registro1 = mock(Registro.class);
+		when(registro1.getPatente()).thenReturn("abc123");
+		centrotest.addObserver(observer);
+		centrotest.registrarFinal("abc123");
+		verify(observer,times(1)).update(centrotest.getRegistros());
 	}
 }
