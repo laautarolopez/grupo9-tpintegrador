72b79e3068db30acf5f8a8ff31551b15094c4c18
Lautaro <Lautaro@LAUTARO-PC>
2020-11-07 20:42:12 -0300

    Infraccion clase

---
+++ src/SEM/Infraccion.java
@@ -0,0 +1,33 @@
+package SEM;
+
+import java.time.LocalDateTime;
+
+public class Infraccion {
+	private String patente;
+	private LocalDateTime fechaYHora;
+	private Inspector inspector;
+	private Zona zona;
+	
+	public Infraccion(String patente, Inspector inspector) {
+		this.patente = patente;
+		this.fechaYHora = LocalDateTime.now();
+		this.inspector = inspector;
+		this.zona = inspector.getZona();
+	}
+	
+	public String getPatente() {
+		return this.patente;
+	}
+	
+	public LocalDateTime getFechaYHora() {
+		return this.fechaYHora;
+	}
+	
+	public Inspector getInspector() {
+		return this.inspector;
+	}
+	
+	public Zona getZona() {
+		return this.zona;
+	}
+}
