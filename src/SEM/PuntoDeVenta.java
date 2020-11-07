596247cc0e96afdbd7934f99a0d956c1e7eebf11
Lautaro <Lautaro@LAUTARO-PC>
2020-11-07 20:42:35 -0300

    Se agrego el monto al punto de venta.

--- src/SEM/PuntoDeVenta.java
+++ src/SEM/PuntoDeVenta.java
@@ -3,13 +3,19 @@ package SEM;
 public class PuntoDeVenta extends PuntoDeCarga implements GeneradorDeRegistros {
 	protected Zona zona;
 	protected CentroRegistros centroRegistros = CentroRegistros.getCentro();
+	protected int monto;
 	
 	public PuntoDeVenta(Zona zona) {
 		this.zona = zona;
+		this.monto = 0;
 	}
 	
 	public void iniciarEstacionamiento(String patente, int monto) {
-		RegistroPuntoDeVenta registro = new RegistroPuntoDeVenta(patente,this.zona, monto);
-		centroRegistros.registrarInicio(registro);
+		int montoExacto = monto / 40 * 40;
+		if (montoExacto >= 40) {
+			this.monto += montoExacto;
+			RegistroPuntoDeVenta registro = new RegistroPuntoDeVenta(patente,this.zona, montoExacto);
+			centroRegistros.registrarInicio(registro);
+		}
 	}
 }
