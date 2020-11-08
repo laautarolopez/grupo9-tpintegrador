2d6bef9213af2042be792abc8faaa1950de93b2c
Lautaro <Lautaro@LAUTARO-PC>
2020-11-07 21:06:24 -0300

    AplicacionInspectorTest recien creado

---
+++ tests/SEM/AplicacionInspectorTest.java
@@ -0,0 +1,14 @@
+package SEM;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AplicacionInspectorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
