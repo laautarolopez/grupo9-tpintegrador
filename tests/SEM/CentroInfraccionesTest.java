ea96ac14da53b2a01d2dd485a52b020a3612a963
Lautaro <Lautaro@LAUTARO-PC>
2020-11-07 21:06:13 -0300

    CentroInfraccionesTest recien creado

---
+++ tests/SEM/CentroInfraccionesTest.java
@@ -0,0 +1,16 @@
+package SEM;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class CentroInfraccionesTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
