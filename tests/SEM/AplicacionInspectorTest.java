d8966227c61bac0d39d9d6ba5ef54ac46348b810
Lautaro <Lautaro@LAUTARO-PC>
2020-11-09 15:00:52 -0300

    AplicacionInspectorTest sin terminar

--- tests/SEM/AplicacionInspectorTest.java
+++ tests/SEM/AplicacionInspectorTest.java
@@ -2,13 +2,29 @@ package SEM;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.*;
+
 public class AplicacionInspectorTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private AplicacionInspector aplicacion;
+	private Inspector inspector;
+	private Registro registro;
+	
+	@BeforeEach
+	public void setUp() {
+		inspector = mock(Inspector.class);
+		registro = mock(Registro.class);
+		aplicacion = new AplicacionInspector(inspector);
 	}
-
+	
+	@Test
+	public void estaVigenteTest() {
+		CentroRegistros centroRegistros = CentroRegistros.getCentro();
+		centroRegistros.registrarInicio(registro);
+		when(registro.estaVigente()).thenReturn(true);
+		when(registro.getPatente()).thenReturn("abc123");
+		assertTrue(aplicacion.estaVigente("abc123"));
+	}
 }
