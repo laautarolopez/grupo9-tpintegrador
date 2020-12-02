675435fe7acb3dd1acb61477573ce36342fbec98
Lautaro <Lautaro@LAUTARO-PC>
2020-12-02 20:50:42 -0300

    CentroInfraccionesTest

--- tests/SEM/CentroInfraccionesTest.java
+++ tests/SEM/CentroInfraccionesTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
-
 public class CentroInfraccionesTest {
 	public CentroInfracciones centroInfracciones;
 	public Infraccion infraccion1;
@@ -16,13 +15,33 @@ public class CentroInfraccionesTest {
 	@BeforeEach
 	public void setUp() {
 		centroInfracciones = new CentroInfracciones();
+		
+		infraccion1 = mock(Infraccion.class);
+		when(infraccion1.getPatente()).thenReturn("ABC123");
+		
+		infraccion2 = mock(Infraccion.class);
+		when(infraccion2.getPatente()).thenReturn("DEF456");
 	}
 	
 	@Test
-	public void agregarInfraccionTest() {
-		infraccion1 = mock(Infraccion.class);
-		when(infraccion1.getPatente()).thenReturn("ABC123");
+	public void registrarInfraccionTest() {
 		centroInfracciones.registrarInfraccion(infraccion1);
 		assertTrue(centroInfracciones.contieneInfraccion(infraccion1.getPatente()));
+		
+		centroInfracciones.registrarInfraccion(infraccion2);
+		assertTrue(centroInfracciones.contieneInfraccion(infraccion2.getPatente()));
+	}
+	
+	@Test
+	public void contieneInfraccionTest() {
+		assertFalse(centroInfracciones.contieneInfraccion(infraccion1.getPatente()));
+		centroInfracciones.registrarInfraccion(infraccion1);
+		assertTrue(centroInfracciones.contieneInfraccion(infraccion1.getPatente()));
+		
+		assertFalse(centroInfracciones.contieneInfraccion(infraccion2.getPatente()));
+		centroInfracciones.registrarInfraccion(infraccion2);
+		assertTrue(centroInfracciones.contieneInfraccion(infraccion2.getPatente()));
+		
+		assertFalse(centroInfracciones.contieneInfraccion("CCC111"));
 	}
 }
